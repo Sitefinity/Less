e385495796285ef957615615ec5c6f6cc08e0b6f
kaloyanpetkov <kaloyan.petkov@telerik.com>
2015-10-27 09:01:45 +0200

    Upgraded to Sitefinity 8.2.5900.0

--- Telerik.Less.Test/TestCompiler.cs
+++ Telerik.Less.Test/TestCompiler.cs
@@ -10,10 +10,7 @@ namespace Telerik.Less.UnitTests
 		public void TestCssOutPut()
 		{
 			string lessFile = Environment.CurrentDirectory + @"\test.less";
-			string output = @"body {
-  background-color: Green;
-}
-";
+			string output = "body{background-color:Green}\n";
 
 			LessCompiler compiler = null;
 			try
@@ -33,5 +30,30 @@ namespace Telerik.Less.UnitTests
 
 			Assert.AreEqual(output, css);
 		}
+
+        [TestMethod]
+        public void TestLessOperations()
+        {
+            string lessFile = Environment.CurrentDirectory + @"\test2.less";
+            string output = "#header{color:#333;border-left:1px;border-right:2px}\n#footer{color:#333}\n";
+
+            LessCompiler compiler = null;
+            try
+            {
+                compiler = new LessCompiler();
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Compiler was not created successfully!");
+            }
+
+            string css = string.Empty;
+            if (compiler != null)
+            {
+                css = compiler.CompileFile(lessFile, true);
+            }
+
+            Assert.AreEqual(output, css);
+        }
 	}
 }
